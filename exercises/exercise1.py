3dec385f04f29385ac5955a9bc6dae2a8b9e1928
jihenerezgui <47503552+jihenerezgui@users.noreply.github.com>
2022-06-07 10:12:28 -0400

    Update exercise1.py

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -4,7 +4,7 @@ from quickHand import HandDetector
 # Pour la capture video:
 # Vous pouvez passez en paramètre une valeur de -1 à 0 pour utiliser une webcam.
 # Vous pouvez donner l'emplacement d'une vidéo. Exemple: "./videoAmusante.mp4" .
-cap = cv2.VideoCapture(-1)
+cap = cv2.VideoCapture(0)
 detector = HandDetector(detectionCon=0.8,maxHands=2)
 
 def main():
